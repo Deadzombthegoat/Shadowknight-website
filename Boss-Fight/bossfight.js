cdb45d4f6d50651b4e84a05933a05fb00237e3cc
Stupidteaman <167263178+Stupidteaman@users.noreply.github.com>
2024-09-14 14:28:28 -0400

    Update bossfight.js
    Signed-off-by: Stupidteaman <167263178+Stupidteaman@users.noreply.github.com>

--- Boss-Fight/bossfight.js
+++ Boss-Fight/bossfight.js
@@ -6,7 +6,7 @@ animate();
 function init() {
     // Scene setup
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
     
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
